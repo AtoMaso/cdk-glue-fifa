4389ec9cff2fafb09a362c66c79166bc7c91f26e
Mirko Srbinovski <srbinovskimirko@gmail.com>
2022-03-11 14:24:24 +1000

    IAM role and s3 bucket

--- lib/cdk-glue-fifa-stack.ts
+++ lib/cdk-glue-fifa-stack.ts
@@ -1,5 +1,8 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as cdk from 'aws-cdk-lib';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkGlueFifaStack extends Stack {
@@ -12,5 +15,37 @@ export class CdkGlueFifaStack extends Stack {
     // const queue = new sqs.Queue(this, 'CdkGlueFifaQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
+
+    // Create a new Role for Glue
+     const role = new iam.Role(this, 'access-glue-fifa', {
+      assumedBy: new iam.ServicePrincipal('glue.amazonaws.com'),
+    });
+    
+    // Add AWSGlueServiceRole to role.
+    const gluePolicy = iam.ManagedPolicy.fromAwsManagedPolicyName("service-role/AWSGlueServiceRole");
+    role.addManagedPolicy(gluePolicy);
+
+
+
+      // Create a new S3 bucket for Glue job.
+      const bucketName = '<ID>-fifa';
+      const dataBucketName = '<ID>-data';
+  
+      const glueS3Bucket = new s3.Bucket(this, 'GlueFifaBucket', {
+        versioned: true,
+        bucketName: bucketName,
+        removalPolicy: cdk.RemovalPolicy.DESTROY,
+        blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+        autoDeleteObjects: true
+      });
+      
+      // Assign role to S3 bucket
+      glueS3Bucket.grantReadWrite(role);
+  
+      // Assign permission to data bucket
+      const dataS3Bucket = s3.Bucket.fromBucketName(this, 'existingBucket', dataBucketName);
+  
+      dataS3Bucket.grantReadWrite(role);
+
   }
 }
